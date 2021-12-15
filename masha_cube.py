785bf51bebe3eeba9c261f23d61933f69ba7b944
mrakhman <robinbad1312@gmail.com>
2021-12-15 18:00:50 +0100

    correct spins

--- masha_cube.py
+++ masha_cube.py
@@ -131,40 +131,40 @@ class Cube_beginner:
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # F -> U
-        self.state['U'][2][0] = prev_state['F'][2][2]
-        self.state['U'][2][1] = prev_state['F'][1][2]
-        self.state['U'][2][2] = prev_state['F'][0][2]
+        self.state['U'][0][2] = prev_state['F'][0][2]
+        self.state['U'][1][2] = prev_state['F'][1][2]
+        self.state['U'][2][2] = prev_state['F'][2][2]
         # D -> F
-        self.state['F'][0][2] = prev_state['D'][0][0]
-        self.state['F'][1][2] = prev_state['D'][0][1]
-        self.state['F'][2][2] = prev_state['D'][0][2]
+        self.state['F'][0][2] = prev_state['D'][0][2]
+        self.state['F'][1][2] = prev_state['D'][1][2]
+        self.state['F'][2][2] = prev_state['D'][2][2]
         # B -> D
-        self.state['D'][0][0] = prev_state['B'][2][0]
-        self.state['D'][0][1] = prev_state['B'][1][0]
         self.state['D'][0][2] = prev_state['B'][0][0]
+        self.state['D'][1][2] = prev_state['B'][1][0]
+        self.state['D'][2][2] = prev_state['B'][2][0]
         # U -> B
-        self.state['B'][0][0] = prev_state['U'][2][0]
-        self.state['B'][1][0] = prev_state['U'][2][1]
+        self.state['B'][0][0] = prev_state['U'][0][2]
+        self.state['B'][1][0] = prev_state['U'][1][2]
         self.state['B'][2][0] = prev_state['U'][2][2]
     
     def right_prime(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # U -> F
-        self.state['F'][2][2] = prev_state['U'][2][0]
-        self.state['F'][1][2] = prev_state['U'][2][1]
-        self.state['F'][0][2] = prev_state['U'][2][2]
+        self.state['F'][0][2] = prev_state['U'][0][2]
+        self.state['F'][1][2] = prev_state['U'][1][2]
+        self.state['F'][2][2] = prev_state['U'][2][2]
         # F -> D
-        self.state['D'][0][0] = prev_state['F'][0][2]
-        self.state['D'][0][1] = prev_state['F'][1][2]
-        self.state['D'][0][2] = prev_state['F'][2][2]
+        self.state['D'][0][2] = prev_state['F'][0][2]
+        self.state['D'][1][2] = prev_state['F'][1][2]
+        self.state['D'][2][2] = prev_state['F'][2][2]
         # D -> B
-        self.state['B'][2][0] = prev_state['D'][0][0]
-        self.state['B'][1][0] = prev_state['D'][0][1]
         self.state['B'][0][0] = prev_state['D'][0][2]
+        self.state['B'][1][0] = prev_state['D'][1][2]
+        self.state['B'][2][0] = prev_state['D'][2][2]
         # B -> U
-        self.state['U'][2][0] = prev_state['B'][0][0]
-        self.state['U'][2][1] = prev_state['B'][1][0]
+        self.state['U'][0][2] = prev_state['B'][0][0]
+        self.state['U'][1][2] = prev_state['B'][1][0]
         self.state['U'][2][2] = prev_state['B'][2][0]
 
     def right_double(self):
@@ -176,41 +176,41 @@ class Cube_beginner:
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # B -> U
+        self.state['U'][0][0] = prev_state['B'][0][2]
+        self.state['U'][1][0] = prev_state['B'][1][2]
         self.state['U'][2][0] = prev_state['B'][2][2]
-        self.state['U'][2][1] = prev_state['B'][1][2]
-        self.state['U'][2][2] = prev_state['B'][0][2]
         # D -> B
         self.state['B'][0][2] = prev_state['D'][0][0]
-        self.state['B'][1][2] = prev_state['D'][0][1]
-        self.state['B'][2][2] = prev_state['D'][0][2]
+        self.state['B'][1][2] = prev_state['D'][1][0]
+        self.state['B'][2][2] = prev_state['D'][2][0]
         # F -> D
-        self.state['D'][0][0] = prev_state['F'][2][0]
-        self.state['D'][0][1] = prev_state['F'][1][0]
-        self.state['D'][0][2] = prev_state['F'][0][0]
+        self.state['D'][0][0] = prev_state['F'][0][0]
+        self.state['D'][1][0] = prev_state['F'][1][0]
+        self.state['D'][2][0] = prev_state['F'][2][0]
         # U -> F
-        self.state['F'][0][0] = prev_state['U'][2][0]
-        self.state['F'][1][0] = prev_state['U'][2][1]
-        self.state['F'][2][0] = prev_state['U'][2][2]
+        self.state['F'][0][0] = prev_state['U'][0][0]
+        self.state['F'][1][0] = prev_state['U'][1][0]
+        self.state['F'][2][0] = prev_state['U'][2][0]
     
     def left_prime(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # U -> B
+        self.state['B'][0][2] = prev_state['U'][0][0]
+        self.state['B'][1][2] = prev_state['U'][1][0]
         self.state['B'][2][2] = prev_state['U'][2][0]
-        self.state['B'][1][2] = prev_state['U'][2][1]
-        self.state['B'][0][2] = prev_state['U'][2][2]
         # B -> D
         self.state['D'][0][0] = prev_state['B'][0][2]
-        self.state['D'][0][1] = prev_state['B'][1][2]
-        self.state['D'][0][2] = prev_state['B'][2][2]
+        self.state['D'][1][0] = prev_state['B'][1][2]
+        self.state['D'][2][0] = prev_state['B'][2][2]
         # D -> F
-        self.state['F'][2][0] = prev_state['D'][0][0]
-        self.state['F'][1][0] = prev_state['D'][0][1]
-        self.state['F'][0][0] = prev_state['D'][0][2]
+        self.state['F'][0][0] = prev_state['D'][0][0]
+        self.state['F'][1][0] = prev_state['D'][1][0]
+        self.state['F'][2][0] = prev_state['D'][2][0]
         # F -> U
-        self.state['U'][2][0] = prev_state['F'][0][0]
-        self.state['U'][2][1] = prev_state['F'][1][0]
-        self.state['U'][2][2] = prev_state['F'][2][0]
+        self.state['U'][0][0] = prev_state['F'][0][0]
+        self.state['U'][1][0] = prev_state['F'][1][0]
+        self.state['U'][2][0] = prev_state['F'][2][0]
 
     def left_double(self):
         self.left()
@@ -220,134 +220,132 @@ class Cube_beginner:
     def back(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
-        # R -> U
-        self.state['U'][2][0] = prev_state['R'][2][2]
-        self.state['U'][2][1] = prev_state['R'][1][2]
-        self.state['U'][2][2] = prev_state['R'][0][2]
-        # D -> R
-        self.state['R'][0][2] = prev_state['D'][0][0]
-        self.state['R'][1][2] = prev_state['D'][0][1]
-        self.state['R'][2][2] = prev_state['D'][0][2]
-        # L -> D
-        self.state['D'][0][0] = prev_state['L'][2][0]
-        self.state['D'][0][1] = prev_state['L'][1][0]
-        self.state['D'][0][2] = prev_state['L'][0][0]
         # U -> L
-        self.state['L'][0][0] = prev_state['U'][2][0]
-        self.state['L'][1][0] = prev_state['U'][2][1]
-        self.state['L'][2][0] = prev_state['U'][2][2]
-    
+        self.state['L'][0][0] = prev_state['U'][0][2]
+        self.state['L'][1][0] = prev_state['U'][0][1]
+        self.state['L'][2][0] = prev_state['U'][0][0]
+        # L -> D
+        self.state['D'][2][0] = prev_state['L'][0][0]
+        self.state['D'][2][1] = prev_state['L'][1][0]
+        self.state['D'][2][2] = prev_state['L'][2][0]
+        # D -> R
+        self.state['R'][2][2] = prev_state['D'][2][0]
+        self.state['R'][1][2] = prev_state['D'][2][1]
+        self.state['R'][0][2] = prev_state['D'][2][2]
+        # R -> U
+        self.state['U'][0][0] = prev_state['R'][0][2]
+        self.state['U'][0][1] = prev_state['R'][1][2]
+        self.state['U'][0][2] = prev_state['R'][2][2]
+
     def back_prime(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
-        # U -> R
-        self.state['R'][2][2] = prev_state['U'][2][0]
-        self.state['R'][1][2] = prev_state['U'][2][1]
-        self.state['R'][0][2] = prev_state['U'][2][2]
-        # R -> D
-        self.state['D'][0][0] = prev_state['R'][0][2]
-        self.state['D'][0][1] = prev_state['R'][1][2]
-        self.state['D'][0][2] = prev_state['R'][2][2]
-        # D -> L
-        self.state['L'][2][0] = prev_state['D'][0][0]
-        self.state['L'][1][0] = prev_state['D'][0][1]
-        self.state['L'][0][0] = prev_state['D'][0][2]
         # L -> U
-        self.state['U'][2][0] = prev_state['L'][0][0]
-        self.state['U'][2][1] = prev_state['L'][1][0]
-        self.state['U'][2][2] = prev_state['L'][2][0]
-
+        self.state['U'][0][0] = prev_state['L'][2][0]
+        self.state['U'][0][1] = prev_state['L'][1][0]
+        self.state['U'][0][2] = prev_state['L'][0][0]
+        # D -> L
+        self.state['L'][0][0] = prev_state['D'][2][0]
+        self.state['L'][1][0] = prev_state['D'][2][1]
+        self.state['L'][2][0] = prev_state['D'][2][2]
+        # R -> D
+        self.state['D'][2][0] = prev_state['R'][2][2]
+        self.state['D'][2][1] = prev_state['R'][1][2]
+        self.state['D'][2][2] = prev_state['R'][0][2]
+        # U -> R
+        self.state['R'][0][2] = prev_state['U'][0][0]
+        self.state['R'][1][2] = prev_state['U'][0][1]
+        self.state['R'][2][2] = prev_state['U'][0][2]
+    
     def back_double(self):
         self.back()
         self.back()
         self.n_spins -= 1 # extract 1 spin because 2 self.back() give us 2 spins, and we want to count double spin as 1 spin
     
-    # TODO: fix spin error
     def up(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # L -> B
-        self.state['B'][2][0] = prev_state['L'][2][2]
-        self.state['B'][2][1] = prev_state['L'][1][2]
-        self.state['B'][2][2] = prev_state['L'][0][2]
+        self.state['B'][0][0] = prev_state['L'][0][0]
+        self.state['B'][0][1] = prev_state['L'][0][1]
+        self.state['B'][0][2] = prev_state['L'][0][2]
         # F -> L
-        self.state['L'][0][2] = prev_state['F'][0][0]
-        self.state['L'][1][2] = prev_state['F'][0][1]
-        self.state['L'][2][2] = prev_state['F'][0][2]
+        self.state['L'][0][0] = prev_state['F'][0][0]
+        self.state['L'][0][1] = prev_state['F'][0][1]
+        self.state['L'][0][2] = prev_state['F'][0][2]
         # R -> F
-        self.state['F'][0][0] = prev_state['R'][2][0]
-        self.state['F'][0][1] = prev_state['R'][1][0]
-        self.state['F'][0][2] = prev_state['R'][0][0]
+        self.state['F'][0][0] = prev_state['R'][0][0]
+        self.state['F'][0][1] = prev_state['R'][0][1]
+        self.state['F'][0][2] = prev_state['R'][0][2]
         # B -> R
-        self.state['R'][0][0] = prev_state['B'][2][0]
-        self.state['R'][1][0] = prev_state['B'][2][1]
-        self.state['R'][2][0] = prev_state['B'][2][2]
+        self.state['R'][0][0] = prev_state['B'][0][0]
+        self.state['R'][0][1] = prev_state['B'][0][1]
+        self.state['R'][0][2] = prev_state['B'][0][2]
 
     def up_prime(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # B -> L
-        self.state['L'][2][2] = prev_state['B'][2][0]
-        self.state['L'][1][2] = prev_state['B'][2][1]
-        self.state['L'][0][2] = prev_state['B'][2][2]
+        self.state['L'][0][0] = prev_state['B'][0][0]
+        self.state['L'][0][1] = prev_state['B'][0][1]
+        self.state['L'][0][2] = prev_state['B'][0][2]
         # L -> F
-        self.state['F'][0][0] = prev_state['L'][0][2]
-        self.state['F'][0][1] = prev_state['L'][1][2]
-        self.state['F'][0][2] = prev_state['L'][2][2]
+        self.state['F'][0][0] = prev_state['L'][0][0]
+        self.state['F'][0][1] = prev_state['L'][0][1]
+        self.state['F'][0][2] = prev_state['L'][0][2]
         # F -> R
-        self.state['R'][2][0] = prev_state['F'][0][0]
-        self.state['R'][1][0] = prev_state['F'][0][1]
-        self.state['R'][0][0] = prev_state['F'][0][2]
+        self.state['R'][0][0] = prev_state['F'][0][0]
+        self.state['R'][0][1] = prev_state['F'][0][1]
+        self.state['R'][0][2] = prev_state['F'][0][2]
         # R -> B
-        self.state['B'][2][0] = prev_state['R'][0][0]
-        self.state['B'][2][1] = prev_state['R'][1][0]
-        self.state['B'][2][2] = prev_state['R'][2][0]
+        self.state['B'][0][0] = prev_state['R'][0][0]
+        self.state['B'][0][1] = prev_state['R'][0][1]
+        self.state['B'][0][2] = prev_state['R'][0][2]
     
     def up_double(self):
         self.up()
         self.up()
         self.n_spins -= 1 # extract 1 spin because 2 self.up() give us 2 spins, and we want to count double spin as 1 spin
     
-    # TODO: fix spin error
     def down(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # L -> F
-        self.state['F'][2][0] = prev_state['L'][2][2]
-        self.state['F'][2][1] = prev_state['L'][1][2]
-        self.state['F'][2][2] = prev_state['L'][0][2]
+        self.state['F'][2][0] = prev_state['L'][2][0]
+        self.state['F'][2][1] = prev_state['L'][2][1]
+        self.state['F'][2][2] = prev_state['L'][2][2]
         # B -> L
-        self.state['L'][0][2] = prev_state['B'][0][0]
-        self.state['L'][1][2] = prev_state['B'][0][1]
-        self.state['L'][2][2] = prev_state['B'][0][2]
+        self.state['L'][2][0] = prev_state['B'][2][0]
+        self.state['L'][2][1] = prev_state['B'][2][1]
+        self.state['L'][2][2] = prev_state['B'][2][2]
         # R -> B
-        self.state['B'][0][0] = prev_state['R'][2][0]
-        self.state['B'][0][1] = prev_state['R'][1][0]
-        self.state['B'][0][2] = prev_state['R'][0][0]
+        self.state['B'][2][0] = prev_state['R'][2][0]
+        self.state['B'][2][1] = prev_state['R'][2][1]
+        self.state['B'][2][2] = prev_state['R'][2][2]
         # F -> R
-        self.state['R'][0][0] = prev_state['F'][2][0]
-        self.state['R'][1][0] = prev_state['F'][2][1]
-        self.state['R'][2][0] = prev_state['F'][2][2]
+        self.state['R'][2][0] = prev_state['F'][2][0]
+        self.state['R'][2][1] = prev_state['F'][2][1]
+        self.state['R'][2][2] = prev_state['F'][2][2]
     
     def down_prime(self):
         self.n_spins += 1
         prev_state = deepcopy(self.state)
         # F -> L
-        self.state['L'][2][2] = prev_state['F'][2][0]
-        self.state['L'][1][2] = prev_state['F'][2][1]
-        self.state['L'][0][2] = prev_state['F'][2][2]
+        self.state['L'][2][0] = prev_state['F'][2][0]
+        self.state['L'][2][1] = prev_state['F'][2][1]
+        self.state['L'][2][2] = prev_state['F'][2][2]
         # L -> B
-        self.state['B'][0][0] = prev_state['L'][0][2]
-        self.state['B'][0][1] = prev_state['L'][1][2]
-        self.state['B'][0][2] = prev_state['L'][2][2]
+        self.state['B'][2][0] = prev_state['L'][2][0]
+        self.state['B'][2][1] = prev_state['L'][2][1]
+        self.state['B'][2][2] = prev_state['L'][2][2]
         # B -> R
-        self.state['R'][2][0] = prev_state['B'][0][0]
-        self.state['R'][1][0] = prev_state['B'][0][1]
-        self.state['R'][0][0] = prev_state['B'][0][2]
+        self.state['R'][2][0] = prev_state['B'][2][0]
+        self.state['R'][2][1] = prev_state['B'][2][1]
+        self.state['R'][2][2] = prev_state['B'][2][2]
         # R -> F
-        self.state['F'][2][0] = prev_state['R'][0][0]
-        self.state['F'][2][1] = prev_state['R'][1][0]
-        self.state['F'][2][2] = prev_state['R'][2][0]
+        self.state['F'][2][0] = prev_state['R'][2][0]
+        self.state['F'][2][1] = prev_state['R'][2][1]
+        self.state['F'][2][2] = prev_state['R'][2][2]
 
     def down_double(self):
         self.down()
@@ -359,7 +357,8 @@ test = Cube_beginner()
 test.print_state()
 
 print('________________________________')
-test.up()
+test.down_prime()
+test.down()
 test.print_state()
 
 print(test.n_spins)
