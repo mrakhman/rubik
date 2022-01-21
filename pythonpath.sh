2654c4738c19c7c2314870d9e377e3a00baabaa5
mrakhman <robinbad1312@gmail.com>
2022-01-21 15:23:06 +0100

    rm pythonpath

--- pythonpath.sh
+++
@@ -1,4 +0,0 @@
-# Modify PYTHONPATH to import filess like modules
-# https://stackoverflow.com/questions/4383571/importing-files-from-different-folder
-
-export PYTHONPATH="${PYTHONPATH}:/sgoinfre/goinfre/Perso/mrakhman/rubik"
