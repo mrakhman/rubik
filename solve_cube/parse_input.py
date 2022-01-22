92ad08597d8499870af5c2f6a72a2f82ebf44c2b
mrakhman <robinbad1312@gmail.com>
2022-01-22 20:05:11 +0100

    write to file step by step

--- solve_cube/parse_input.py
+++ solve_cube/parse_input.py
@@ -9,11 +9,13 @@ def give_valid_moves_example():
     print("To test with random string of moves run `python3 file_name.py random N` where 1 <= N <= 300")
 
 
-def write_to_file(shuffle_spins, solving_spins):
+def write_to_file(shuffle_spins, *solving_spins):
     f = open("shuffle_solving_spins.txt", "w")
     f.write(" ".join(shuffle_spins))
     f.write("\n")
-    f.write(" ".join(solving_spins))
+    for step in solving_spins:
+        f.write(" ".join(step))
+        f.write("\n")
     f.close()
 
 
--- solve_step_by_step.py
+++ solve_step_by_step.py
@@ -9,8 +9,7 @@ def print_spins_inline(spins):
             print(spin, end =" ")
         print()
 
-
-def print_this_step_moves(prev_step_moves_len, solver):
+def print_this_step_moves(prev_step_moves_len, solver, arr_of_steps):
     solver.remove_extra_spins()
     current_step_moves = solver.solving_moves
     current_step_moves_len = len(current_step_moves)
@@ -18,6 +17,7 @@ def print_this_step_moves(prev_step_moves_len, solver):
     print()
     print_spins_inline(current_step_moves[prev_step_moves_len:])
     print()
+    arr_of_steps.append(current_step_moves[prev_step_moves_len:])
 
     cube.print_state()
     print("__________________________________________________\n")
@@ -25,6 +25,7 @@ def print_this_step_moves(prev_step_moves_len, solver):
 
 
 if __name__ == "__main__":
+    arr_of_steps = []
     cube = Cube()
     shuffle_spins = parse_user_input()
     if shuffle_spins:
@@ -39,36 +40,36 @@ if __name__ == "__main__":
 
         print("Step 1 - solve white cross:\n")
         solver.step_1()
-        step_1_moves_len = print_this_step_moves(0, solver)
+        step_1_moves_len = print_this_step_moves(0, solver, arr_of_steps)
 
 
         print("Step 2 - solve white side:\n")
         solver.step_2()
-        step_2_moves_len = print_this_step_moves(step_1_moves_len, solver)
+        step_2_moves_len = print_this_step_moves(step_1_moves_len, solver, arr_of_steps)
 
 
         print("Step 3 - second layer:\n")
         solver.step_3()
-        step_3_moves_len = print_this_step_moves(step_2_moves_len, solver)
+        step_3_moves_len = print_this_step_moves(step_2_moves_len, solver, arr_of_steps)
 
 
         print("Step 4 - yellow cross:\n")
         solver.step_4()
-        step_4_moves_len = print_this_step_moves(step_3_moves_len, solver)
+        step_4_moves_len = print_this_step_moves(step_3_moves_len, solver, arr_of_steps)
 
 
         print("Step 5 - yellow side:\n")
         solver.step_5()
-        step_5_moves_len = print_this_step_moves(step_4_moves_len, solver)
+        step_5_moves_len = print_this_step_moves(step_4_moves_len, solver, arr_of_steps)
 
 
         print("Step 6 - final step - Edges on third layer centers:\n")
         solver.step_6()
-        step_6_moves_len = print_this_step_moves(step_5_moves_len, solver)
+        step_6_moves_len = print_this_step_moves(step_5_moves_len, solver, arr_of_steps)
 
 
         print("Total number of spins:", len(solver.solving_moves))
         print()
         solver.print_solving_spins()
 
-        write_to_file(shuffle_spins, solver.solving_moves)
+        write_to_file(shuffle_spins, *arr_of_steps)
